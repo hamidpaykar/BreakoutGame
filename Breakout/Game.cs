c062d118743915e5ca5e8b2183ce2685a218d6aa
MichaelGhandforoush <89693483+MichaelGhandforoush@users.noreply.github.com>
2024-04-23 18:21:15 +0200

    Added Game and Program

--- Breakout/Game.cs
+++ Breakout/Game.cs
@@ -1,4 +1,3 @@
-namespace Breakout.Game;
 
 using System.Collections.Generic;
 using DIKUArcade;
@@ -6,35 +5,28 @@ using DIKUArcade.Entities;
 using DIKUArcade.GUI;
 using DIKUArcade.Input;
 using DIKUArcade.Graphics;
+using System;
+
+namespace Breakout
+{
+    
 
 public class Game : DIKUGame {
 
     public Game(WindowArgs windowArgs) : base(windowArgs) {
         window.SetKeyEventHandler(KeyHandler);
-        shape = new StationaryShape(0.5f, 0.5f, 0.5f, 0.5f);
-        strides = ImageStride.CreateStrides(4, @"Assets/PuffOfSmoke.png");
-        animation = new Animation() {Duration = 0};
     }
 
     private void KeyHandler(KeyboardAction action, KeyboardKey key) {
-        if (action != KeyboardAction.KeyPress) {
-            return;
-        }
-
-        switch (key) {
-            case KeyboardKey.Space:
-                // do something to test animations
-                animation = new Animation() {Duration = 1000, Shape = shape};
-                animation.Stride = new ImageStride(40, strides);
-                break;
-            case KeyboardKey.Escape:
-                window.CloseWindow();
-                break;
-        }
+        Console.WriteLine("");
     }
 
     public override void Render() { 
+                Console.WriteLine("");        
     }
 
-    public override void Update() { }
+    public override void Update() {
+        Console.WriteLine("");
+     }
+}
 }
